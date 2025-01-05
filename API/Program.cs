657a48e73636fe914567c1d18aeae49ff52d5c37
Robert <trajcevr@yahoo.com>
2025-01-05 23:12:29 +0100

    Added Angular CLI

--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,53 @@
-using System;
-using Microsoft.EntityFrameworkCore;
+// using System;
+// using Microsoft.EntityFrameworkCore;
 
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+
+// builder.Services.AddControllers();
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+// builder.Services.AddDbContext<API.Data.DataContext>(options =>
+//     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+
+// app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http//localhost:4200"));
+
+// app.UseAuthorization();
+
+// app.MapControllers();
+
+// app.Run();
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add the DbContext for your application, connecting to SQLite.
 builder.Services.AddDbContext<API.Data.DataContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
@@ -24,10 +58,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Enable HTTPS redirection.
 app.UseHttpsRedirection();
 
+// CORS Configuration
+app.UseCors(x => x.AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .WithOrigins("http://localhost:4200"));  // Allow Angular frontend
+
+// Enable Authorization (if you are using authentication and authorization)
 app.UseAuthorization();
 
+// Map controllers to handle HTTP requests
 app.MapControllers();
 
+// Run the application
 app.Run();
