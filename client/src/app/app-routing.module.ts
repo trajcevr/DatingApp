cd448045b6d9cc3cc2f39047a3596238d08f3e4b
Robert <trajcevr@yahoo.com>
2025-01-08 22:11:18 +0100

    added auth guard :)

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,15 +5,24 @@ import { ListsComponent } from './lists/lists.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MessagesComponent } from './messages/messages.component'; // Import the component
+import { AuthGuard } from './_guards/auth.guard';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'members', component: MemberListComponent },
-  { path: 'members/:id', component: MemberDetailComponent },
-  { path: 'lists', component: ListsComponent },
-  { path: 'messages', component: MessagesComponent },  // Ensure this is set
-  { path: '**', component: HomeComponent }
+  {
+    path: '',
+    runGuardsAndResolvers: "always",
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
+      { path: 'members/:id', component: MemberDetailComponent },
+      { path: 'lists', component: ListsComponent },
+      { path: 'messages', component: MessagesComponent }, 
+      { path: '**', component: HomeComponent }
+    ]
+  }
+  
 ];
 
 @NgModule({
