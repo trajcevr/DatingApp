71fdd847991fcc2b18b331c6579e605d9fab7379
Robert <trajcevr@yahoo.com>
2025-01-13 18:09:12 +0100

    building the user interface

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,16 +1,8 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
-
-const user = localStorage.getItem('user');
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: user ? 'Bearer ' + JSON.parse(user).token : ''
-  })
-};
 
 @Injectable({
   providedIn: 'root'
@@ -22,20 +14,13 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member []>(this.baseUrl + 'users', httpOptions)
+    return this.http.get<Member []>(this.baseUrl + 'users')
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions)
+    return this.http.get<Member>(this.baseUrl + 'users/' + username)
   }
 }
 
 
-// getMembers() {
-//   return this.http.get(this.baseUrl + 'users').pipe(
-//     catchError(error => {
-//       console.error('Error fetching members:', error);
-//       return throwError(() => error); // Handle or rethrow the error
-//     })
-//   );
-// }
+
